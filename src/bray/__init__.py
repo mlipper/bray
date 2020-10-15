e27f78d3583a6d15734b64d5812bffb7eebf1ad9
Matthew Lipper <mlipper@gmail.com>
2020-10-15 17:27:10 -0400

    Add module-level logging init and logger

--- src/bray/__init__.py
+++ src/bray/__init__.py
@@ -1,1 +1,15 @@
+__all__ = ['cli', 'config', 'errors', 'main']
+__author__ = 'mlipper'
 __version__ = '0.1.0'
+
+import logging
+import sys
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
+    level=logging.DEBUG,
+    datefmt="%H:%M:%S",
+    stream=sys.stderr,
+)
+#logging.getLogger("chardet.charsetprober").disabled = True
+logger = logging.getLogger(__name__)
