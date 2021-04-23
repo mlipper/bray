8b11adb61d665fbe74a69901b6840748e61ea91b
Matthew Lipper <mlipper@gmail.com>
2021-04-23 16:09:02 -0400

    Default module logging config

--- src/bray/__init__.py
+++ src/bray/__init__.py
@@ -29,15 +29,10 @@ def configure_logging(conf=LOGGING, levels=LOGLEVELS):
             print(f"props={props}")
             props['level'] = levels[name]
     logging.config.dictConfig(conf)
-    # Return conf, levels for easier testing
-    return conf, levels
-
-
-def _init():
-    conf, __ = configure_logging()
     logger = logging.getLogger(__name__)
     logger.debug("%s logger configured by %s using:", __name__, "logging.config.dictConfig")
-    logger.debug(conf)
+    global _bray_logging
+    _bray_logging = (conf, levels)
 
 
-_init()
+configure_logging()
--- tests/test_bray.py
+++ tests/test_bray.py
@@ -1,12 +1,17 @@
 import logging
 
 
+from bray import _bray_logging
+from bray.config import LOGGING, LOGLEVELS
+
+
 logger = logging.getLogger(__name__)
 
 
-def test_configure_logging():
-    # TODO test me!
-    logger.warning("TODO: test me!!")
+def test_default_module_logging():
+    conf, levels = _bray_logging
+    assert conf is LOGGING
+    assert levels is LOGLEVELS
 
 
 def test_main():
